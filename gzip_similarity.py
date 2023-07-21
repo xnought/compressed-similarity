28caf39e21af81fb1d5b35d8e334eb3c57e8445d
xnought <bertuccd@oregonstate.edu>
2023-07-21 14:34:42 -0400

    feat: gzip similarity example

--- gzip_similarity.py
+++ gzip_similarity.py
@@ -1,13 +1,20 @@
 import gzip
 
 
-def compressed_length(a: str):
-    return len(gzip.compress(a.encode()))
+def compressed_size(message: str, compressor=gzip):
+    return len(compressor.compress(message.encode()))
+
+
+def normalized_compression_distance(
+    a_compressed_size: int, b_compressed_size: int, ab_compressed_size: int
+):
+    return (ab_compressed_size - min(a_compressed_size, b_compressed_size)) / max(
+        a_compressed_size, b_compressed_size
+    )
 
 
 def gzip_similarity(a: str, b: str):
-    a_len = compressed_length(a)
-    b_len = compressed_length(b)
-    ab_len = compressed_length(a + " " + b)
-    normalized = (ab_len - min(a_len, b_len)) / max(a_len, b_len)
-    return normalized
+    a_size = compressed_size(a)
+    b_size = compressed_size(b)
+    ab_size = compressed_size(a + " " + b)
+    return normalized_compression_distance(a_size, b_size, ab_size)
