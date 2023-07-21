5ecd657bd9e911ad6badfeaf9168f34bafeadf39
xnought <bertuccd@oregonstate.edu>
2023-07-21 14:52:43 -0400

    feat: make more general because I'm an insane person

--- gzip_similarity.py
+++ gzip_similarity.py
@@ -1,20 +1,25 @@
 import gzip
+from typing import Callable
 
 
-def compressed_size(message: str, compressor=gzip):
-    return len(compressor.compress(message.encode()))
+def ncd(a: bytes, b: bytes, compressor: Callable[[bytes], bytes]) -> float:
+    """ncd normalized compression distance
 
+    Args:
+        a: string 1
+        b: string 2
+        compressor: compress bytes into less bytes
 
-def normalized_compression_distance(
-    a_compressed_size: int, b_compressed_size: int, ab_compressed_size: int
-):
+    Returns:
+        distance:s similarity from a to b from 0.0 to 1.0
+    """
+    a_compressed_size = len(compressor(a))
+    b_compressed_size = len(compressor(b))
+    ab_compressed_size = len(compressor(a + b" " + b))
     return (ab_compressed_size - min(a_compressed_size, b_compressed_size)) / max(
         a_compressed_size, b_compressed_size
     )
 
 
 def gzip_similarity(a: str, b: str):
-    a_size = compressed_size(a)
-    b_size = compressed_size(b)
-    ab_size = compressed_size(a + " " + b)
-    return normalized_compression_distance(a_size, b_size, ab_size)
+    return ncd(a.encode(), b.encode(), gzip.compress)
