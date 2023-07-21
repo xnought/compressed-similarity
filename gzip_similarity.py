ed7484cee3b659cb6aaf3735d2878950b0c227e6
xnought <bertuccd@oregonstate.edu>
2023-07-21 15:00:28 -0400

    stop optimizing you dumby. cant helpmyself

--- gzip_similarity.py
+++ gzip_similarity.py
@@ -2,8 +2,20 @@ import gzip
 from typing import Callable
 
 
-def ncd(a: bytes, b: bytes, compressor: Callable[[bytes], bytes]) -> float:
-    """ncd normalized compression distance
+space_byte = " ".encode()
+
+
+def space_separated_concat(a: bytes, b: bytes):
+    return a + space_byte + b
+
+
+def normalized_compression_distance(
+    a: bytes,
+    b: bytes,
+    compressor: Callable[[bytes], bytes],
+    concat_bytes: Callable[[bytes, bytes], bytes] = space_separated_concat,
+) -> float:
+    """normalized compression distance
 
     Args:
         a: string 1
@@ -11,15 +23,15 @@ def ncd(a: bytes, b: bytes, compressor: Callable[[bytes], bytes]) -> float:
         compressor: compress bytes into less bytes
 
     Returns:
-        distance:s similarity from a to b from 0.0 to 1.0
+        distance: similarity from a to b from 0.0 to 1.0
     """
     a_compressed_size = len(compressor(a))
     b_compressed_size = len(compressor(b))
-    ab_compressed_size = len(compressor(a + b" " + b))
+    ab_compressed_size = len(compressor(concat_bytes(a, b)))
     return (ab_compressed_size - min(a_compressed_size, b_compressed_size)) / max(
         a_compressed_size, b_compressed_size
     )
 
 
 def gzip_similarity(a: str, b: str):
-    return ncd(a.encode(), b.encode(), gzip.compress)
+    return normalized_compression_distance(a.encode(), b.encode(), gzip.compress)
